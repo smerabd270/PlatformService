a407321bd0e6d971abbaa3c95fe2f705defd5ec7
AmerAbd <amerabd270@gmail.com>
2025-07-12 03:06:32 +0300

    t

--- PlatformService/IPlatformRepo.cs
+++ PlatformService/IPlatformRepo.cs
@@ -1,6 +1,13 @@
-﻿namespace PlatformService
+﻿using PlatformService.Models;
+
+namespace PlatformService
 {
     public interface IPlatformRepo
     {
+        bool SaveChanges();
+        IEnumerable<IPlatformRepo> GetAllPlatforms();
+        Platform GetPlatformBId(int bId); 
+        void CreatePlatform(Platform plat);
+
     }
 }
---
+++ PlatformService/PlatformRepo.cs
@@ -0,0 +1,37 @@
+﻿using PlatformService.Data;
+using System.Linq
+
+using PlatformService.Models;
+
+namespace PlatformService
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _context;
+
+        public PlatformRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreatePlatform(Platform plat)
+        {
+            _context.Platforms.Add(plat);
+        }
+
+        public IEnumerable<IPlatformRepo> GetAllPlatforms()
+        {
+            return (IEnumerable<IPlatformRepo>)_context.Platforms.ToList();
+        }
+
+        public Platform GetPlatformBId(int bId)
+        {
+            return _context.Platforms.FirstOrDefault(x => x.Id == bId);
+        }
+
+        public bool SaveChanges()
+        {
+           return (_context.SaveChanges()>=0);
+        }
+    }
+}
