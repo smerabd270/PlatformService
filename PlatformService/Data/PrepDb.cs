e9b16f471147abe2ffe597b28c2880c4d10760b0
AmerAbd <amerabd270@gmail.com>
2025-07-13 02:21:29 +0300

    add dto

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -1,8 +1,10 @@
-﻿namespace PlatformService.Data
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
 {
     public static class PrepDb
     {
-        public static void PrepPopulation( IApplicationBuilder app)
+        public static void PrepPopulation(IApplicationBuilder app)
         {
             using (var serviceScoped = app.ApplicationServices.CreateScope())
             {
@@ -11,7 +13,17 @@
         }
         private static void SeedData(AppDbContext context)
         {
-            if (!context.Platforms.Any()) { }
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("----> Seed data");
+                context.Platforms.AddRange(
+                    new Platform { Name = "Dot Net", Publisher = "microsoft", Cost = "free" },
+                    new Platform { Name = "Sql server Express", Publisher = "microsoft", Cost = "free" },
+                    new Platform { Name = "kubernetes", Publisher = "cloud native computing fundation", Cost = "free" }
+                    );
+                context.SaveChanges();
+
+            }
             else
             {
                 Console.WriteLine("------> We alreadyhvae data");
